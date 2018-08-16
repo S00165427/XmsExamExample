84f9caca064fb4663e4329e64c3e8e2ee2389350
rubabramzan96 <rubabramzan96@gmail.com>
2018-08-16 15:51:18 +0100

    initial commit

---
+++ XmsExam/XmsExam/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(XmsExam.Startup))]
+namespace XmsExam
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
