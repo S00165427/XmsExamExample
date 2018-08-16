5603c3c3eea87207a9bb356a7683d9ff67ae1548
rubabramzan96 <rubabramzan96@gmail.com>
2018-08-16 16:12:00 +0100

    Q1a - set code first classes and add data context

---
+++ XmsExam/XmsExam/Models/LibraryContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace XmsExam.Models
+{
+    public class LibraryContext: DbContext
+    {
+        public LibraryContext() : base("DefaultConnection")
+        {
+            Configuration.LazyLoadingEnabled = false;
+        }
+        public DbSet<Member> Members { get; set; }
+        public DbSet<Loan> Loans { get; set; }
+        public DbSet<Book> books { get; set; }
+    }
+
+}
+}
